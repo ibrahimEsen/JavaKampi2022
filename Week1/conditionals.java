d030f754a992f4de6233f20c0470161b9ddb5369
IEM <iemandaci@hotmail.com>
2022-10-14 10:21:17 +0300

    .

---
+++ Week1/conditionals.java
@@ -0,0 +1,17 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int sayi = 3;
+		if (sayi<11) {
+			
+			System.out.println("sayi 11'den KÜÇÜKTÜR");			
+		}else if (sayi==11) {
+			System.out.println("Sayı 11'e EŞİTTİR");
+		}else {
+			System.out.println("Sayı 11'den BÜYÜKTÜR");
+		}
+
+	}
+
+}
