90dc4bc08839cd6c60052a7c28dcd555f748c6e2
IEM <iemandaci@hotmail.com>
2022-10-14 09:39:55 +0300

    .

--- Week2/methods/src/methods/Main.java
+++ Week2/methods/src/methods/Main.java
@@ -5,37 +5,32 @@ public class Main {
 	public static void main(String[] args) {
 		sayiBul();
 		sayiBul();
-
 	}
-	
+
 	public static void sayiBul() {
 
-
-		int [] numbers = new int [] {1,2,5,7,9,0};
+		int[] numbers = new int[] { 1, 2, 5, 7, 9, 0 };
 		int find = 2;
 		boolean isThere = false;
-		
-		
+
 		for (int number : numbers) {
-			if(number==find) {
-				isThere=true;
+			if (number == find) {
+				isThere = true;
 				break;
 			}
 		}
-		
+
 		String textMessage = "";
-		if(isThere) {
-			textMessage =find + " sayısı listede mevcuttur";
+		if (isThere) {
+			textMessage = find + " sayısı listede mevcuttur";
 			outMessage(textMessage);
 		} else {
-			textMessage =find + " sayısı listede mevcut değildir";
+			textMessage = find + " sayısı listede mevcut değildir";
 			outMessage(textMessage);
 		}
-		
 	}
- 
+
 	public static void outMessage(String mesaj) {
 		System.out.println(mesaj);
-	}	
-	
+	}
 }
