7757a3e9738ea1e42c2ee28f1d3b8bd6e09d86da
adgsy <adgsy@192.168.1.137>
2022-11-03 13:35:17 +0300

    Lessons 24-37

---
+++ Week2/methods.java
@@ -0,0 +1,34 @@
+public class Main {
+
+	public static void main(String[] args) {
+		sayiBul();
+		sayiBul();
+	}
+
+	public static void sayiBul() {
+
+		int[] numbers = new int[] { 1, 2, 5, 7, 9, 0 };
+		int find = 2;
+		boolean isThere = false;
+
+		for (int number : numbers) {
+			if (number == find) {
+				isThere = true;
+				break;
+			}
+		}
+
+		String textMessage = "";
+		if (isThere) {
+			textMessage = find + " sayısı listede mevcuttur";
+			outMessage(textMessage);
+		} else {
+			textMessage = find + " sayısı listede mevcut değildir";
+			outMessage(textMessage);
+		}
+	}
+
+	public static void outMessage(String mesaj) {
+		System.out.println(mesaj);
+	}
+}
