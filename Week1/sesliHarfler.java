27cc0fcee82af78530e1c068295fdef0999aa6c8
adgsy <adgsy@192.168.1.137>
2022-11-03 13:28:37 +0300

    Lesson 1-23

---
+++ Week1/sesliHarfler.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		char harf = 'A';
+		
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println(harf + " harfi kalın sesli harftir");
+			break;
+			default:
+				System.out.println(harf + " harfi ince sesli harftir");		
+		}
+	}
+}
