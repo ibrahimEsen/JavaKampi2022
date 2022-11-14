030a6b31d5f3f9cac0a7505f0fab3065b4d81529
adgsy <adgsy@192.168.1.137>
2022-11-14 09:17:04 +0300

    .

--- Week1/conditionals.java
+++ Week1/conditionals.java
@@ -11,6 +11,19 @@ public class Main {
 		}else {
 			System.out.println("Sayı 11'den BÜYÜKTÜR");
 		}
+		
+		int not = 51;
+		//50 ve üzeri puan ise GEÇTİ
+		//40-49 arası ise BÜTÜNLEME
+		//0-39 arası ise KALDI
+					
+		if (not<=39) {
+			System.out.println("KALIR");
+		} else if (not>=40 && not<50) {
+			System.out.println("BÜTÜNLEME");
+		} else {
+			System.out.println("GEÇER");
+		}	
 
 	}
 
