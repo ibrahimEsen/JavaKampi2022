adee94e303fe5d28d216db4026a4082d044aa62e
adgsy <adgsy@D16>
2022-12-18 17:14:53 +0300

    Lesson 40

---
+++ Week3/abstractClasses/src/abstractClasses/Main.java
@@ -0,0 +1,14 @@
+package abstractClasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		KidsGameCalculator kidsGameCalculator = new KidsGameCalculator();
+		kidsGameCalculator.calculate();
+		kidsGameCalculator.gameOver();
+		
+		GameCalculator gameCalculator = new ManGameCalculator();
+	}
+
+}
