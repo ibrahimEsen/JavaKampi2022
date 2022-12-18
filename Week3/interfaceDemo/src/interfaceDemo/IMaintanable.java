af02bb020c3ff5d3605142f1f18764bb3b259c7d
adgsy <adgsy@D16>
2022-12-18 23:08:46 +0300

    Lesson 44

---
+++ Week3/interfaceDemo/src/interfaceDemo/IMaintanable.java
@@ -0,0 +1,7 @@
+package interfaceDemo;
+
+public interface IMaintanable {
+	
+	void maintance();
+
+}
---
+++ Week3/interfaceDemo/src/interfaceDemo/RobotWorker.java
@@ -0,0 +1,17 @@
+package interfaceDemo;
+
+public class RobotWorker implements IWorkable, IMaintanable{
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maintance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
